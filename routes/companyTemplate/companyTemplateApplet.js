1ae15d85092f2b6b5980cc8c5c77d3a06bdcb200
fanzehua <316679581@qq.com>
2022-03-30 17:48:02 +0800

    修正参数判断

--- routes/companyTemplate/companyTemplateApplet.js
+++ routes/companyTemplate/companyTemplateApplet.js
@@ -6,10 +6,15 @@ const CompanyTemplate = require("../../models/companyTemplateSchema");
 const util = require("../../utils/util");
 const log4j = require("../../utils/log4");
 const { default: mongoose } = require("mongoose");
+const { CODE } = require("../../utils/util");
 
 router.post("/applet/template", async (ctx) => {
   try {
-    const { companyId = "" } = ctx.request.body;
+    const { companyId } = ctx.request.body;
+    if (!companyId) {
+      ctx.body = util.fail("", "参数错误", CODE.PARAM_ERROR);
+      return;
+    }
     const res = await CompanyTemplate.findOne({ companyId: new mongoose.Types.ObjectId(companyId), type: "2" });
     ctx.body = util.success(res ?? {});
   } catch (error) {
