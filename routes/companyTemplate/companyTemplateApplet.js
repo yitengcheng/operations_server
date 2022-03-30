a44e2def45e55410824798acc1848997cb190cf1
fanzehua <316679581@qq.com>
2022-03-30 17:43:15 +0800

    修正参数

--- routes/companyTemplate/companyTemplateApplet.js
+++ routes/companyTemplate/companyTemplateApplet.js
@@ -5,11 +5,12 @@ const router = require("koa-router")();
 const CompanyTemplate = require("../../models/companyTemplateSchema");
 const util = require("../../utils/util");
 const log4j = require("../../utils/log4");
+const { default: mongoose } = require("mongoose");
 
 router.post("/applet/template", async (ctx) => {
   try {
     const { companyId = "" } = ctx.request.body;
-    const res = await CompanyTemplate.findOne({ companyId, type: "2" });
+    const res = await CompanyTemplate.findOne({ companyId: new mongoose.Types.ObjectId(companyId), type: "2" });
     ctx.body = util.success(res ?? {});
   } catch (error) {
     ctx.body = util.fail(error.stack);
