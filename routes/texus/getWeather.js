82b1e8f39ebe8106d06eebf38aa6f15086f8aeec
fanzehua <316679581@qq.com>
2023-01-09 17:38:42 +0800

    修正天气接口

--- routes/texus/getWeather.js
+++ routes/texus/getWeather.js
@@ -7,7 +7,9 @@ const util = require('../../utils/util');
 
 router.post('/test/getWeather', async (ctx) => {
   try {
-    let ip = ctx.request.ip;
+    console.log('------ip', ctx?.request?.ip);
+    console.log('------header', ctx?.request?.['x-forwarded-for']);
+    let ip = ctx?.request?.ip || ctx?.request?.['x-forwarded-for'];
     const gdKey = '10d0f72d75258bae2bf4341ab981eda2';
     const ipResponse = await axios.get(`https://restapi.amap.com/v3/ip?ip=${ip}&output=json&key=${gdKey}`);
     const adcode = ipResponse?.data?.adcode;
