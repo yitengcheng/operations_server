08ac86678641a1d933601a375f549ee68ae8c936
fanzehua <316679581@qq.com>
2023-01-14 13:51:30 +0800

    添加库管说明书，修正天气接口

--- routes/texus/getWeather.js
+++ routes/texus/getWeather.js
@@ -7,16 +7,13 @@ const util = require('../../utils/util');
 
 router.post('/test/getWeather', async (ctx) => {
   try {
-    console.log('------ip', ctx?.request?.ip);
     let ip = ctx?.request?.ip;
     const gdKey = '10d0f72d75258bae2bf4341ab981eda2';
     const ipResponse = await axios.get(`https://restapi.amap.com/v3/ip?ip=${ip}&output=json&key=${gdKey}`);
-    console.log('------ipResponse', ipResponse?.data);
     const adcode = ipResponse?.data?.adcode;
     const weatherResponse = await axios.get(
       `https://restapi.amap.com/v3/weather/weatherInfo?city=${adcode}&output=json&key=${gdKey}&extensions=base`,
     );
-    console.log('------weatherResponse', weatherResponse?.data);
     if (weatherResponse?.data?.lives) {
       ctx.body = util.success(weatherResponse?.data?.lives[0]);
     } else {
