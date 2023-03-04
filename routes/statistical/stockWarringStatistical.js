04f5261fd3c61a749176a2cd2560f41ca48250ff
fanzehua <316679581@qq.com>
2023-03-04 17:06:22 +0800

    修正BUG

--- routes/statistical/stockWarringStatistical.js
+++ routes/statistical/stockWarringStatistical.js
@@ -9,10 +9,12 @@ const util = require('../../utils/util');
 
 router.post('/statistical/stockWarringStatistical', async (ctx) => {
   const { user } = ctx.state;
-  const goods = await goodsSchema.find(
-    { belongs: user?.belongs ?? user._id, delFlag: false },
-    { name: 1, inventoryNumber: 1, inventoryMax: 1, inventoryMin: 1 },
-  );
+  const goods = await goodsSchema
+    .find(
+      { belongs: user?.belongs ?? user._id, delFlag: false },
+      { name: 1, inventoryNumber: 1, inventoryMax: 1, inventoryMin: 1, classification: 1 },
+    )
+    .populate([{ path: 'classification' }]);
   ctx.body = util.success(goods);
 });
 
